9ed14fefa190308d5ba03e25bc2dcb40b864c78c
Sergey <grushevskiy.1993@gmail.com>
2022-10-13 21:41:43 +0700

    homework47

---
+++ homework_47/Program.cs
@@ -0,0 +1,40 @@
+﻿void homework47()
+{
+    // Задача 47. Задайте двумерный массив размером m×n,
+    // заполненный случайными вещественными числами.
+
+    Console.Clear();
+    Console.Write("Введите m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[m, n];
+    FillArray(array);
+    PrintArray(array);
+
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i, j], 2) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(-100, 100) * 0.15;
+        }
+    }
+}
